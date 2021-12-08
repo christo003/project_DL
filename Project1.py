1666309e7dc50cd0a202a50a8808dcbe1db03316
leo <leo.vigna@etu.unige.ch>
2021-12-08 12:51:50 +0100

    Add iteration for cross validation. Add weight initialisation according to the coursweight initialisation according to the courweight initialisation according to the coursweight initialisation according to the courseesee Added result display

--- Project1.py
+++ Project1.py
@@ -9,7 +9,7 @@ from dlc_practical_prologue import *
 from matplotlib import pyplot as plt
 
 import torchvision
-from comparisonNets import comparisonNet1, comparisonNet2
+from comparisonNets import comparisonNet, comparisonNetWS, comparisonNetWSAuxLoss
 print(len(generate_pair_sets(1)))
 
 import sklearn
@@ -75,6 +75,7 @@ plt.show()
 
 #%% Building training set
 
+
 dataset = generate_pair_sets(1000)
 
 train_image = dataset[train_input_id].float()
@@ -86,24 +87,24 @@ test_target = dataset[test_target_id].float()
 test_classes = dataset[test_classes_id].float()
 
 
-
+nbr_iteration = 10
 
 
 #%% Utils
 
 def evaluateAccuracy(dataloader,model):
     incorrect_count = 0
-    for img_input,target,test in iter(dataloader):        
+    for img_input,target,test in iter(dataloader):  
+        mean,std = img_input.mean(),img_input.std()
+        img_input.sub_(mean).div_(std)
         output = model(img_input)
-        if isinstance(output,tuple):
-            output = output[2]#trick because one time we have one output, and after we have three output...to optimize
+        if isinstance(output,tuple):#ugly trick because one time we have one output, and after we have three output...to optimize
+            output = output[2]
         incorrect_count += sum(abs(target.view(-1,1)-output.round()))
         
 
     return (1-incorrect_count/len(dataloader.dataset))*100
 
-#%% Training the model
-print('\n ### Training Start ### \n')
 
 
 dataset = list(zip(train_image,train_target,train_classes))
@@ -114,92 +115,183 @@ dataset_test = list(zip(test_image,test_target,test_classes))
 
 batch_size = 100
 
-compnet1 = comparisonNet1()
-optimizer1 = torch.optim.Adam(compnet1.parameters(), lr=0.001)
-
-
-criterion1 = torch.nn.BCELoss()
-
 
 train_loader = DataLoader(dataset=dataset_train, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(dataset=dataset_val, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=dataset_test, batch_size=batch_size, shuffle=True)
 
-start_training = time.time()
-epochs = 25
-for e in range(epochs):
-    # for idx,img_input in enumerate(train_image):
-    print('current epoch : %i/%i' % (e,epochs))
-    for img_input,target,target_digit in iter(train_loader):
-        
-        output = compnet1(img_input)
-        
-        loss = criterion1(output,target)
-        compnet1.zero_grad()#reset the gradient for each mini-batch
-        loss.backward()
-        optimizer1.step()
-    
-    
-    
-    print('Validation : %f %%' % (evaluateAccuracy(val_loader,compnet1)))
-    #Il manque l'étape de validation a la fin de l'epochs
-    
-stop_training = time.time()
 
-print('\nTraining took : %f seconds \n' % (stop_training-start_training))
+#%% Training the model
+print('\n ### Training Start with 1st architecture : ### \n')
+
+
+compnet1 = comparisonNet()
+optimizer1 = torch.optim.Adam(compnet1.parameters(), lr=0.001)
+criterion1 = torch.nn.BCELoss()
+
+
+cross_val_1 = []
+for i in range(nbr_iteration):
+    start_training = time.time()
+    epochs = 25
+    val_acc_1 = []
+    compnet1.initParameter() #Reset the parameters before each training
+    for e in range(epochs):
+        # for idx,img_input in enumerate(train_image):
+        print('current epoch : %i/%i' % (e+1,epochs))
+        for img_input,target,target_digit in iter(train_loader):
+            
+            #Normalization of the input batch
+            mean,std = img_input.mean(),img_input.std()
+            img_input.sub_(mean).div_(std)
+            
+            #Forward pass
+            output = compnet1(img_input)
+            
+            
+            #Loss calculation and weight updates
+            loss = criterion1(output,target)
+            compnet1.zero_grad()#reset the gradient for each mini-batch
+            loss.backward()
+            optimizer1.step()
+        
+        
+        val_acc = evaluateAccuracy(val_loader,compnet1)
+        val_acc_1.append(val_acc)
+        print('Validation accuracy : %f %%' % (val_acc))
+        
+    
+    stop_training = time.time()
+    cross_val_1.append(evaluateAccuracy(val_loader,compnet1))
+    print('\nTraining took : %f seconds, iteration %i / %i \n' % (stop_training-start_training,i+1,nbr_iteration))
 #%% Test of the model
 
 print('\n ### Testing Start ### \n')
 
-print('Final score : %f %%' % (evaluateAccuracy(test_loader, compnet1)))
+print('Final score 1st architecture : %f %%' % (evaluateAccuracy(test_loader, compnet1)))
 
+time.sleep(2)
 
 #%% Training of 2nd architecture
 
-print('\n ### Training Start ### \n')
+print('\n ### Training Start with weight sharing ### \n')
 
-compnet2 = comparisonNet2()
-criterion_digit_1 = torch.nn.BCELoss()
-criterion_digit_2 = torch.nn.BCELoss()
+compnet2 = comparisonNetWS()
 criterion2 = torch.nn.BCELoss()
-optimizer2 = torch.optim.Adam(compnet2.parameters(), lr=0.001)
+#list(model1.parameters()) + list(model2.parameters()
+# optimizer2 = torch.optim.Adam(list(compnet2.parameters())+list(compnet2.commonNet.parameters()), lr=0.001)
+optimizer2 = torch.optim.Adam(compnet2.parameters(), lr = 0.001)
 
-start_training = time.time()
-epochs = 50
-for e in range(epochs):
-    # for idx,img_input in enumerate(train_image):
-    print('current epoch : %i/%i' % (e,epochs))
-    for img_input,target,target_digit in iter(train_loader):
-        
-        output1,output2,output = compnet2(img_input)
-        
-        target_digit1_oh = F.one_hot(target_digit[:,0].to(torch.int64)).float()
-        target_digit2_oh = F.one_hot(target_digit[:,1].to(torch.int64)).float()
+cross_val_2 = []
+for i in range(nbr_iteration):
 
+    start_training = time.time()
+    epochs = 25
+    val_acc_2 = []
+    # compnet2.commonNet.initParameter()#reset the parameters uniformly of the common network
+    compnet2.initParameter()#reset the parameters uniformly 
+    for e in range(epochs):
+        # for idx,img_input in enumerate(train_image):
+        print('current epoch : %i/%i' % (e+1,epochs))
+        for img_input,target,target_digit in iter(train_loader):
+            
+            #Normalization of the input batch
+            mean,std = img_input.mean(),img_input.std()
+            img_input.sub_(mean).div_(std)
+            
+            #Forware pass
+            output = compnet2(img_input)
+            
+            #Loss calculation and weight updates
+            loss = criterion2(output,target)        
+            compnet2.zero_grad()#reset the gradient for each mini-batch
+            loss.backward()
+            optimizer2.step()
         
-        loss_digit_1 = criterion_digit_1(output1,target_digit1_oh)
-        loss_digit_2 = criterion_digit_2(output2,target_digit2_oh)
-        loss_class = criterion2(output,target)        
-        loss_final = loss_digit_1+loss_digit_2+loss_class
         
-        compnet2.zero_grad()#reset the gradient for each mini-batch
-        loss_final.backward()
-        optimizer2.step()
+        val_acc = evaluateAccuracy(val_loader,compnet2)
+        val_acc_2.append(val_acc)
+        print('Validation accuracy : %f %%' % (val_acc))
+        
+    stop_training = time.time()
     
-    
-    
-    print('Validation : %f %%' % (evaluateAccuracy(val_loader,compnet2)))
-    #Il manque l'étape de validation a la fin de l'epochs
-    
-stop_training = time.time()
-
-print('\nTraining took : %f seconds \n' % (stop_training-start_training))
+    cross_val_2.append(evaluateAccuracy(val_loader,compnet2))
+    print('\nTraining took : %f seconds, iteration %i / %i \n' % (stop_training-start_training,i+1,nbr_iteration))
 
 #%% Test of the model
 print('\n ### Testing Start ### \n')
-print('Final score : %f %%' % (evaluateAccuracy(test_loader, compnet2)))
+print('Final score with weigth sharing : %f %%' % (evaluateAccuracy(test_loader, compnet2)))
 
 #%% 3rd architecture
 
+
+print('\n ### Training Start with weight sharing and auxiliary Loss ### \n')
+
+compnet3 = comparisonNetWSAuxLoss()
+criterion_digit_1 = torch.nn.BCELoss()
+criterion_digit_2 = torch.nn.BCELoss()
+criterion3 = torch.nn.BCELoss()
+optimizer3 = torch.optim.Adam(compnet3.parameters(), lr=0.001)
+
+
+epochs = 40
+
+cross_val_3 = []
+for i in range(nbr_iteration):
+    start_training = time.time()
+    
+    # compnet3.commonNet.initParameter()#reset the parameters uniformly of the common network
+    compnet3.initParameter()#reset the parameters uniformly 
+    val_acc_3 = []
+    for e in range(epochs):
+        # for idx,img_input in enumerate(train_image):
+        print('current epoch : %i/%i' % (e+1,epochs))
+        for img_input,target,target_digit in iter(train_loader):
+            
+            #Normalization of the input batch
+            mean,std = img_input.mean(),img_input.std()
+            img_input.sub_(mean).div_(std)
+            
+            #Forward pass
+            output1,output2,output = compnet3(img_input)
+            
+            #Loss calculation and weight updates
+            target_digit1_oh = F.one_hot(target_digit[:,0].to(torch.int64)).float()
+            target_digit2_oh = F.one_hot(target_digit[:,1].to(torch.int64)).float()
+            
+            
+            loss_digit_1 = criterion_digit_1(output1,target_digit1_oh)
+            loss_digit_2 = criterion_digit_2(output2,target_digit2_oh)
+            loss_class = criterion3(output,target)        
+            loss_final = loss_digit_1+loss_digit_2+loss_class
+            
+            compnet3.zero_grad()#reset the gradient for each mini-batch
+            loss_final.backward()
+            optimizer3.step()
+        
+        
+        val_acc = evaluateAccuracy(val_loader,compnet3)
+        val_acc_3.append(val_acc)
+        print('Validation accuracy : %f %%' % (val_acc))
+        #Il manque l'étape de validation a la fin de l'epochs
+        
+    stop_training = time.time()
+    cross_val_3.append(evaluateAccuracy(val_loader,compnet3))
+    print('\nTraining took : %f seconds, iteration %i / %i \n' % (stop_training-start_training,i+1,nbr_iteration))
+
+#%% Test of the model
+print('\n ### Testing Start ### \n')
+print('Final score with weigth sharing and axiliary loss : %f %%' % (evaluateAccuracy(test_loader, compnet3)))
+
+#%% Results
+
+fig,ax = plt.subplots()
+ax.set_title('Cross validation')
+x = range(1,11)
+ax.set_ylim([0, 100])
+ax.set_xlabel('iteration')
+ax.set_ylabel('% accuracy')
+ax.plot(x,cross_val_1,x,cross_val_2,x,cross_val_3)
+ax.legend(('classic conv net','weight sharing','weight sharing + auxiliary loss'))
 
 
--- comparisonNets.py
+++ comparisonNets.py
@@ -3,18 +3,24 @@
 import torch
 import torchvision
 import torch.nn as nn
-
+import math
 import torch.nn.functional as F
 
-class comparisonNet1(nn.Module):
+class comparisonNet(nn.Module):
+    """
+    This modele simply use convolutional layer and extract features in order
+    to predict if one number is bigger than the other.
+    
+    It consider the input as a 2 channel image.
+    """
     def __init__(self):
-        super(comparisonNet1,self).__init__()
-        conv1_channel = 32
-        conv2_channel = 64
-        self.conv1 = nn.Conv2d(2,conv1_channel,2)
-        self.conv1_bn=nn.BatchNorm2d(conv1_channel)
-        self.conv2 = nn.Conv2d(conv1_channel,conv2_channel,4)
-        self.conv2_bn=nn.BatchNorm2d(conv2_channel)
+        super(comparisonNet,self).__init__()
+        self.conv1_channel = 32
+        self.conv2_channel = 64
+        self.conv1 = nn.Conv2d(2,self.conv1_channel,2)
+        self.conv1_bn=nn.BatchNorm2d(self.conv1_channel)
+        self.conv2 = nn.Conv2d(self.conv1_channel,self.conv2_channel,4)
+        self.conv2_bn=nn.BatchNorm2d(self.conv2_channel)
         self.flat = nn.Flatten()
         
         
@@ -34,11 +40,103 @@ class comparisonNet1(nn.Module):
         x = F.sigmoid(self.fc2(x))
         # print(x.shape)
         return x
+
+    def initParameter(self):
+        self.conv1.weight.data = (torch.rand(self.conv1.weight.size())-0.5)*(1/math.sqrt(2))
+        self.conv2.weight.data = (torch.rand(self.conv2.weight.size())-0.5)*(1/math.sqrt(self.conv1_channel))
+        self.fc1.weight.data = (torch.rand(self.fc1.weight.size())-0.5)*(1/math.sqrt(1600))
+        self.fc2.weight.data = (torch.rand(self.fc2.weight.size())-0.5)*(1/math.sqrt(20))
         
 
-class comparisonNet2(nn.Module):
+class  commonNet(nn.Module):
     def __init__(self):
-        super(comparisonNet2,self).__init__()
+        super(commonNet,self).__init__()
+        self.conv1_channel = 16
+        self.conv2_channel = 32
+        self.conv1 = nn.Conv2d(1,self.conv1_channel,2)#2
+        self.conv1_bn = nn.BatchNorm2d(self.conv1_channel)
+        self.conv2 = nn.Conv2d(self.conv1_channel,self.conv2_channel,4)
+        self.conv2_bn = nn.BatchNorm2d(self.conv2_channel)
+
+        self.flat = nn.Flatten()
+        self.fc1 = nn.Linear(800,10)
+        
+        
+    def forward(self,x):
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = F.relu(F.max_pool2d(self.conv2_bn(self.conv2(x)),kernel_size = 2))
+        x = F.softmax(self.fc1(self.flat(x)))
+        return x
+    
+    def initParameter(self):
+        self.conv1.weight.data = (torch.rand(self.conv1.weight.size())-0.5)*(1/math.sqrt(1))
+        self.conv2.weight.data = (torch.rand(self.conv2.weight.size())-0.5)*(1/math.sqrt(self.conv1_channel))
+        self.fc1.weight.data = (torch.rand(self.fc1.weight.size())-0.5)*(1/math.sqrt(800))
+            
+
+
+
+class comparisonNetWS(nn.Module):
+    """
+    This modele use a sub network that allow to classifiy each number before make the 
+    decision if one number is bigger than the other.
+    
+    The input of each network is a number with 1 channel.
+    """
+    def __init__(self):
+        super(comparisonNetWS,self).__init__()
+        self.conv1_channel = 16
+        self.conv2_channel = 32
+        self.commonNet = nn.Sequential(
+            nn.Conv2d(1,self.conv1_channel,2),#2
+            nn.BatchNorm2d(self.conv1_channel),
+            nn.ReLU(),
+            nn.Conv2d(self.conv1_channel,self.conv2_channel,4),#4
+            nn.BatchNorm2d(self.conv2_channel),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Flatten(),
+            nn.Linear(800,10),
+            nn.Softmax()
+            )
+        # self.commonNet = commonNet()
+        self.fc1 = nn.Linear(20,30)
+        self.fc2 = nn.Linear(30,1)
+                
+    def forward(self,x):
+        number1 = x[:,0,:,:].view(-1,1,14,14)
+        number2 = x[:,1,:,:].view(-1,1,14,14)
+        out1 = self.commonNet(number1)
+        #out1 len(out1) = 10
+        out2 = self.commonNet(number2)
+        
+        # Final loss = gamma*MSE1 + gamma*MSE2 + alpha*FINAL DECISION ()
+        # print(out1.shape)
+        # print(out2.shape)
+        # print(out1.flatten().shape)
+        # print(out2.flatten().shape)
+        out = torch.cat((out1,out2),1)
+        # print(out.shape)
+        out = F.relu(self.fc1(out))
+        out = F.sigmoid(self.fc2(out))
+        
+        return out
+    
+    def initParameter(self):
+        self.fc1.weight.data = (torch.rand(self.fc1.weight.size())-0.5)*(1/math.sqrt(20))
+        self.fc2.weight.data = (torch.rand(self.fc2.weight.size())-0.5)*(1/math.sqrt(30))
+
+class comparisonNetWSAuxLoss(nn.Module):
+    """
+    This modele use a sub network that allow to classifiy each number before make the 
+    decision if one number is bigger than the other.
+    
+    In addition to that the forward method output the intermediate result
+    in order to build a loss that take into account the capacity of the network
+    to find the number in each channel.
+    """
+    def __init__(self):
+        super(comparisonNetWSAuxLoss,self).__init__()
         conv1_channel = 16
         conv2_channel = 32
         self.commonNet = nn.Sequential(
@@ -53,6 +151,7 @@ class comparisonNet2(nn.Module):
             nn.Linear(800,10),
             nn.Softmax()
             )
+        # self.commonNet = commonNet()
         self.fc1 = nn.Linear(20,30)
         self.fc2 = nn.Linear(30,1)
                 
@@ -75,10 +174,10 @@ class comparisonNet2(nn.Module):
         
         return out1,out2,out
     
+    def initParameter(self):
+        self.fc1.weight.data = (torch.rand(self.fc1.weight.size())-0.5)*(1/math.sqrt(20))
+        self.fc2.weight.data = (torch.rand(self.fc2.weight.size())-0.5)*(1/math.sqrt(30))        
     
-class numberRecognizerNet(nn.Module):
-    def __init__(self):
-        super(numberRecognizerNet,self).__init__()
-        self.conv1 = nn.Conv2d(2,10,2)
-        self.conv2 = nn.Conv2d(10,20,4)
+    
+
         
